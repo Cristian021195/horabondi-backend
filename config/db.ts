24ffc1517924f18210536ca375ed8f1190fbc256
cristian021195 <cristiangramajo015@gmail.com>
2022-10-28 21:31:30 -0300

    agregamos .env a gitignore, y variables de entorno de bbdd a railway

--- config/db.ts
+++ config/db.ts
@@ -1,17 +1,11 @@
 import { createPool } from "mysql2/promise";
 
 export const pool = createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    port:3306,
-    database:'horabondi'
-
-    /*host:process.env.DB_PROD_HOST,
+    host:process.env.DB_PROD_HOST,
     user:process.env.DB_PROD_USERNAME,
     password:process.env.DB_PROD_PASSWORD,
     database:process.env.DB_PROD_DATABASE,
     ssl: {
         rejectUnauthorized: false
-    }*/
+    }
 })
